f5464958ee3eea2c907723ed3dd665a58e0fcabd
ghostff <frankchris@hotmail.com>
2018-06-01 22:00:00 -0500

    Added commit helper and completed task disabling

--- Editor/Todo.cs
+++ Editor/Todo.cs
@@ -13,6 +13,7 @@ public class Todo : ScriptableObject
         public int type;
         public bool show;
         public bool done;
+        public bool deleted;
         public string description;
     }
 
@@ -22,6 +23,7 @@ public class Todo : ScriptableObject
 public class TodoEditor : EditorWindow
 {
     private string dir = "Assets/Scripts/Editor/Resources/";
+    private string branch = "origin master";
     private Todo target;
     private string description;
     private string errorMsg;
@@ -31,6 +33,7 @@ public class TodoEditor : EditorWindow
     private Vector2 scrollPos;
     private bool edit;
     private int show;
+    private bool showCommit;
 
     [MenuItem ("Window/Todo %l")]
     public static void Init()
@@ -70,18 +73,37 @@ public class TodoEditor : EditorWindow
         return s;
     }
 
-    private Color GetColor(int type)
+    private Color GetColor(int type, byte alpha = 255)
     {
-        if (type == 3) //urgent
-            return Color.black;
-        else if (type == 2) // InProgress
-            return Color.yellow;
-        else if (type == 1) // Normal
-            return Color.white;
+        if (type == 1) //Feature
+            return new Color32(255, 255, 255, alpha); //white
+        else if (type == 2) // Enhance
+            return new Color32(51, 153, 255, alpha); //sky blue
+        else if (type == 3) // Modify
+            return new Color32(10, 40, 90, alpha); // dark blue
+        else if (type == 4) // Bug
+            return new Color32(255, 0, 0, alpha); // red
         else if (type == 5) // Test
-            return Color.green;
-        else // Bug
-            return Color.red;
+            return new Color32(0, 255, 0, alpha); // green
+        else // active (first)
+            return new Color32(255, 255, 0, alpha); //yellow
+    }
+
+    private string ConCat(int type, string title)
+    {
+        string t = "\n-";
+        if (type == 1) //Feature
+            t += "New: ";
+        else if (type == 2) // Enhance
+            t += "Enhanced: ";
+        else if (type == 3) // Modify
+            t += "Refactored: ";
+        else if (type == 4) // Bug
+            t += "Fixed: ";
+        else if (type == 5) // Test
+            t += "Tested: ";
+
+        return t + title;
     }
 
     private void OnGUI()
@@ -102,12 +124,13 @@ public class TodoEditor : EditorWindow
                 });
                 Save();
             }
-            
         }
 
         string[] types = new string[6] {
-            "All", "Normal", "InProgress", "Urgent", "bug", "Test"	
+            "All", "Feature", "Enhance", "Modify", "Bug", "Test"	
         };
+
+        string commitMsg = "Changes:";
 
         GUIStyle margin = new GUIStyle(GUI.skin.button);
         margin.margin = new RectOffset(0, 0, 0, 0);
@@ -118,13 +141,13 @@ public class TodoEditor : EditorWindow
             GUILayout.BeginVertical("box");
 
                 int size = target.todo.Count - 1;
-                int newSize = size;
+                int last = 0;
                 int completed= 0;
                 bool hasTask = false;
-                for (int i = 0; i <= newSize; i++)
+                for (int i = 0; i <= size; i++)
                 {
-                    if (target.todo[i].done)
-                        newSize--;
+                    if (! target.todo[i].done)
+                        last = i;
                 }
                 
                 GUILayout.BeginHorizontal();
@@ -134,15 +157,19 @@ public class TodoEditor : EditorWindow
                 
                 scrollPos = GUILayout.BeginScrollView(scrollPos,false,true);
                 
-                GUILayout.BeginVertical("box");	
+                GUILayout.BeginVertical("box");
+                    bool first = true;
+                    int lastId = 0;
                     for (int i = 0; i <= size; i++)
                     {
                         TD td = target.todo[i];
-
+                        if (td.deleted)
+                            continue;
+                            
                         if (td.done)
                         {
+                            commitMsg += ConCat(td.type, td.title);
                             completed++;
-                            newSize++;
                             continue;
                         }
                         else if (show != 0 && show != td.type)
@@ -157,7 +184,7 @@ public class TodoEditor : EditorWindow
                             GUILayout.BeginHorizontal();
                         
                                 td.done = EditorGUILayout.Toggle(td.done, GUILayout.Width(20));
-                                EditorGUILayout.LabelField(td.title, FontAndWith(12, 100, GetColor(td.type), FontStyle.Bold));
+                                EditorGUILayout.LabelField(td.title, FontAndWith(12, 100, GetColor(first ? -1 : td.type), FontStyle.Bold));
                                 types[0] = null;
                                 td.type = EditorGUILayout.Popup(td.type, types, GUILayout.Width(100));
                                 if(GUILayout.Button((editing == i) ? "Cancel" : "Edit", margin))
@@ -179,10 +206,10 @@ public class TodoEditor : EditorWindow
                                 }
                                 
                                 TD next = target.todo.ElementAtOrDefault(i + 1);
-                                TD prev = target.todo.ElementAtOrDefault(i - 1);
+                                TD prev = target.todo.ElementAtOrDefault(lastId);
                                 margin.fixedWidth = 20;
 
-                                if(i == 0)
+                                if(first)
                                 {
                                     if(GUILayout.Button("⇓", margin))
                                     {
@@ -194,7 +221,7 @@ public class TodoEditor : EditorWindow
                                         }
                                     }
                                 }
-                                else if(i == newSize)
+                                else if(i == last)
                                 {
                                     if(GUILayout.Button("⇑", margin))
                                     {
@@ -234,13 +261,11 @@ public class TodoEditor : EditorWindow
                                 Color c = margin.normal.textColor;
                                 margin.normal.textColor = Color.red;
                                 if(GUILayout.Button("x", margin))
-                                {
                                     if (EditorUtility.DisplayDialog("Warning!", "Are you sure you want to delete \"" + td.title + "\"",  "Yes", "No"))
                                     {
                                         target.todo.RemoveAt(i);
                                         return;
                                     }
-                                }
                                 margin.normal.textColor = c;
 
                                 margin.fixedWidth = 44;
@@ -249,9 +274,12 @@ public class TodoEditor : EditorWindow
                             if (td.show)
                             {
                                 GUILayout.BeginVertical("box");
-                                    EditorGUILayout.LabelField(td.description, FontAndWith(12, 0, GetColor(td.type), FontStyle.Bold, true));
+                                    EditorGUILayout.LabelField(td.description, FontAndWith(12, 0, GetColor(first ? -1 : td.type), FontStyle.Bold, true));
                                 GUILayout.EndVertical();
                             }
+
+                            first = false;
+                            lastId = i;
 
                         GUILayout.EndVertical();
                     }
@@ -261,30 +289,56 @@ public class TodoEditor : EditorWindow
                         EditorGUILayout.LabelField("No Task", EditorStyles.largeLabel);
                     }
                 GUILayout.EndVertical();
-
-                GUILayout.Label("COMPLETED TASKS:", EditorStyles.boldLabel);
+                
                 if (completed > 0)
                 {
+                    GUILayout.Space(10);
+                
+                    GUILayout.BeginHorizontal();
+                        GUILayout.Label("COMPLETED TASKS:", EditorStyles.boldLabel);
+                        if(GUILayout.Button(showCommit ? "Hide commit message" : "Show commit message", GUILayout.Width(150)))
+                            showCommit = ! showCommit;
+
+                        bool delete = false;
+                        if(GUILayout.Button("Delete Completed", GUILayout.Width(150)))
+                            if (EditorUtility.DisplayDialog("Warning!", "Are you sure you want to delete all completed tasks?",  "Yes", "No"))
+                                delete = true;
+                    
+                    GUILayout.EndHorizontal();
                     GUILayout.BeginVertical("box");
+                        if (showCommit)
+                        {
+                            GUILayout.BeginVertical("box");
+                                commitMsg = "git add . && git commit -m \"" + commitMsg + "\" && git push " + branch;
+                                EditorGUILayout.SelectableLabel(commitMsg, GUILayout.MaxHeight((completed + 1) * 14));
+                            GUILayout.EndVertical();
+                        }
+                        
                         for (int i = 0; i < target.todo.Count; i++)
                         {
                             TD td = target.todo[i];
-                            if (! td.done)
+                            if (! td.done || td.deleted)
                                 continue;
+                            
+                            if (delete)
+                                td.deleted = true;
 
                             GUILayout.BeginVertical("box");
 
                                 GUILayout.BeginHorizontal();
                                     td.done = EditorGUILayout.Toggle(td.done, GUILayout.Width(20));
-                                    EditorGUILayout.LabelField(td.title, FontAndWith(12, 100, Color.grey, FontStyle.Bold));
+                                    EditorGUILayout.LabelField(td.title, FontAndWith(12, 100, GetColor(td.type, 100), FontStyle.Bold));
                                 GUILayout.EndHorizontal();
 
                             GUILayout.EndVertical();
                         }
+
+                        if (delete)
+                            return;
                     GUILayout.EndVertical();
                 }
 
-                GUILayout.EndScrollView ();
+                GUILayout.EndScrollView();
             GUILayout.EndVertical();
         GUILayout.EndArea();
         
@@ -322,6 +376,7 @@ public class TodoEditor : EditorWindow
                             type = this.type,
                             description = this.description
                         };
+                        type = 1;
                         title = "";
                         description = "";
                         GUI.FocusControl(null);
@@ -350,5 +405,6 @@ public class TodoEditor : EditorWindow
     void OnDestroy()
     {
         Save();
-    }	    
+    }	
+    
 }
--- README.md
+++ README.md
@@ -3,6 +3,7 @@ A no frills todo list tool, ideal for small prototypes, game and personal projec
 
 ![alt text](ss.png)
 
+Git branch setup: ``TodoEditor.branch = "origin master"``
 Note: This will create a directory like this ``Assets/Scripts/Editor/Resources/`` in your project,
 unless changed. See:   
-`` EditorWindow.dir = "Assets/Scripts/Editor/Resources/"``
+`` TodoEditor.dir = "Assets/Scripts/Editor/Resources/"``
